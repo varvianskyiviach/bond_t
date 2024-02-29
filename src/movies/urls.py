20a54e9957c97c761a10e90525084d9bae02077d
viacheslav <vyacheslav.idz@gmail.com>
2024-02-29 20:25:52 +0200

    templates update

--- src/movies/urls.py
+++ src/movies/urls.py
@@ -1,10 +1,20 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+from movies import views
 from movies.api import MovieAPISet
 
 router = DefaultRouter()
 router.register(r"movies", MovieAPISet, basename="movies")
 urlpatterns = [
     path("api/v1/", include(router.urls)),
+]
+
+
+urlpatterns += [
+    path("movies/", views.MovieListView.as_view(), name="movie_list"),
+    path("movies/<int:pk>/", views.MovieDetailView.as_view(), name="movie_detail"),
+    path("movies/create/", views.MovieCreateView.as_view(), name="movie_create"),
+    path("movies/<int:pk>/update/", views.MovieUpdateView.as_view(), name="movie_update"),
+    path("movies/<int:pk>/delete/", views.MovieDeleteView.as_view(), name="movie_delete"),
 ]
--- src/movies/views.py
+++ src/movies/views.py
@@ -1,3 +1,46 @@
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView,
+)
+
+from .models import Movie
+
 # from django.shortcuts import render
 
-# Create your views here.
+# # def movie_detail_page(request, pk):
+# #     movie = Movie.objects.get(pk=pk)
+# #     return render(request, 'movies/movie_detail.html', {"movie": movie})
+
+
+class MovieListView(ListView):
+    model = Movie
+    template_name = "movies/movie_list.html"
+
+
+class MovieDetailView(DetailView):
+    model = Movie
+    template_name = "movies/movie_detail.html"
+
+
+class MovieCreateView(CreateView):
+    model = Movie
+    fields = ["title", "release_year", "directors", "actors"]
+    template_name = "movies/movie_form.html"
+    success_url = reverse_lazy("movie_list")
+
+
+class MovieUpdateView(UpdateView):
+    model = Movie
+    fields = ["title", "release_year", "directors", "actors"]
+    template_name = "movies/movie_form.html"
+    success_url = reverse_lazy("movie_list")
+
+
+class MovieDeleteView(DeleteView):
+    model = Movie
+    template_name = "movies/movie_confirm_delete.html"
+    success_url = reverse_lazy("movie_list")
