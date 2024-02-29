365323f7877d2e6b89b4d30226164b16ac0ab493
viacheslav <vyacheslav.idz@gmail.com>
2024-02-29 15:47:26 +0200

    update prev commit

--- src/movies/admin.py
+++ src/movies/admin.py
@@ -2,19 +2,21 @@ from django.contrib import admin
 
 from movies.models import Movie, Person
 
+
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    
+
     def get_directors(self, obj):
         return ", ".join([director.name for director in obj.directors.all()])
-    
+
     def get_actors(self, obj):
         return ", ".join([actor.name for actor in obj.actors.all()])
 
-    list_display = ['id', 'title', 'get_directors', 'get_actors', 'release_year']
-    search_fields = ['title', 'directors__name', 'actors__name', 'release_year']
+    list_display = ["id", "title", "get_directors", "get_actors", "release_year"]
+    search_fields = ["title", "directors__name", "actors__name", "release_year"]
+
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
 
-    list_display = ['id', 'name']
+    list_display = ["id", "name"]
